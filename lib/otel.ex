161f2dfbc5f23785ba1d528c5d1faf226ebaa90d
yangbancode <259762912+yangbancode@users.noreply.github.com>
2026-04-09 01:59:05 +0900

    chore: initialize mix project with mise config (#2)

---
+++ lib/otel.ex
@@ -0,0 +1,18 @@
+defmodule Otel do
+  @moduledoc """
+  Documentation for `Otel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Otel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
